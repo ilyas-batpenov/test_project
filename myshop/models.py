c2cea5ca2efc9ab672ec658bc8673b5e2b2cc919
Ильяс Батпенов <ilyas.batpenov@gmail.com>
2022-04-11 14:18:40 +0600

    create models Shop, Category, Product

--- myshop/models.py
+++ myshop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Shop(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Shop name')
+
+
+class Category(models.Model):
+    category = models.CharField(max_length=100, verbose_name='Category')
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Product name')
+    category = models.ForeignKey('myshop.Category', on_delete=models.RESTRICT,
+                                 related_name='products',
+                                 verbose_name='Product category')
+    shop = models.ForeignKey('myshop.Shop', on_delete=models.CASCADE, related_name='products',
+                             verbose_name='Shop')
