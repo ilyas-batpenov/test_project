d361cf5bf45be92ae7db285502137f596710777c
Ильяс Батпенов <ilyas.batpenov@gmail.com>
2022-04-11 16:52:12 +0600

    add update method to ProductSerializer to count updates

--- shop_api/serializers.py
+++ shop_api/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
-from myshop.models import Shop, Category, Product
+from myshop.models import Product
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'name']
+        fields = ['id', 'name', 'update_counter']
+        read_only_fields = ['update_counter']
 
-        # read_only_fields = ['category', 'shop']
+    def update(self, instance, validated_data):
+        for key, value in validated_data.items():
+            setattr(instance, key, value)
+        instance.update_counter += 1
+        instance.save()
+        return instance
+
+
